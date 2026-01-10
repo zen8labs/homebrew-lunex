e7563fb5dfb62181a31cd50b634d00ef1d564bd6
Nguyễn Khắc Thành <nguyenkhacthanh244@gmail.com>
2026-01-11 00:43:56 +0700

    Update nexo.rb

--- Casks/nexo.rb
+++ Casks/nexo.rb
@@ -1,7 +1,7 @@
 cask "nexo" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.1.0-alpha.25"
+  version "0.1.0-alpha.26"
   sha256 :no_check
 
   url "https://github.com/Nexo-Agent/nexo/releases/download/#{version}/Nexo_#{version}_#{arch}.dmg"
